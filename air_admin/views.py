ae71555e4e98a1aeb5fadfa21573b27e4c56bece
ashish kumar pandey <ashish@ashishs-MacBook-Air.local>
2022-07-09 22:51:19 +0530

    csrf_exempt POST method

--- air_admin/views.py
+++ air_admin/views.py
@@ -1,13 +1,16 @@
 from django.http import JsonResponse
 from rest_framework import status
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
 def create_flight(request):
     if request.method != 'POST':
         return JsonResponse({"msg": "{} method not allowed!!!".format(request.method)}, 
             status= status.HTTP_405_METHOD_NOT_ALLOWED)
     return JsonResponse({"msg": "Add Flight"})
 
+@csrf_exempt
 def update_flight(request, id):
     if request.method != 'PUT':
         return JsonResponse({"msg": "{} method not allowed!!!".format(request.method)}, 
