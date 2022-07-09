db61af02724645730823781876242317a1ef6b11
ashish kumar pandey <ashish@ashishs-MacBook-Air.local>
2022-07-09 22:07:36 +0530

    Rename admin to air_admin, Add Flight model

---
+++ air_admin/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Flight(models.Model):
+    number= models.CharField(max_length= 64)
+    departure_city= models.CharField(max_length= 128)
+    departure_time= models.DateTimeField()
+    arrival_city= models.CharField(max_length= 128)
+    arrival_time= models.DateTimeField()
---
+++ air_admin/views.py
@@ -0,0 +1,4 @@
+from django.http import JsonResponse
+from rest_framework import status
+
+# Create your views here.
